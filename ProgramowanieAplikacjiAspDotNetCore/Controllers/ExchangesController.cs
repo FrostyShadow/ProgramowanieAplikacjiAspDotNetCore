c36a0f1719c946182f00c3f400cd15be62821bcd
Filip Kosmala <kosmala.filip@gmail.com>
2020-10-11 17:46:55 +0200

    Add Exchanges Controller

---
+++ ProgramowanieAplikacjiAspDotNetCore/Controllers/ExchangesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProgramowanieAplikacjiAspDotNetCore.Controllers
+{
+    public class ExchangesController : Controller
+    {
+        public IActionResult Show()
+        {
+            return View();
+        }
+    }
+}
